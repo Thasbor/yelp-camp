74c1e1d963c4541cff0c2496bd82bad49bf8f679
Jeff Goin <goin.jeffrey@gmail.com>
2021-02-07 16:39:43 -0800

    REST  complete

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require('method-override');
 const app = express();
 const Campground = require('./models/campground');
 
-methodOverride('_method');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -23,6 +23,9 @@ db.once('open', () => {
 app.set('view engine', 'ejs')
 app.set('vies', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+
 
 
 app.get('/', (req, res) => {
@@ -35,10 +38,17 @@ app.get('/campgrounds', async (req,res) => {
     res.render('campgrounds', {campgrounds})
 })
 
-app.get('campgrounds/new', (req, res) => {
+app.get('/campgrounds/new', (req, res) => {
     
     res.render('campgrounds/new')
 })
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 
 app.get('/campgrounds/:id', async (req, res) => {
     const {id} = req.params;
@@ -46,11 +56,28 @@ app.get('/campgrounds/:id', async (req, res) => {
     
     res.render('campgrounds/show', {camp} )
 
+})
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    
+    const campground = await Campground.findById(req.params.id) ;
+    res.render(`campgrounds/edit`, {campground})
 })
-app.put('/campgrounds/:id', async (req, res) => {
-    const update = await Campground.findOneAndUpdate()
-})
+
+app.put('/campgrounds/:id', async (req,res) => {
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {useFindAndModify: false})
+    res.redirect(`${id}`)
+ })
+
+ app.delete('/campgrounds/:id', async (req, res) => {
+     const {id} = req.params;
+    const campground = await Campground.findByIdAndDelete(id, {useFindAndModify: false})
+    res.redirect('/campgrounds')
+ })
+
+
+
 
 
 
